956a98dd2e14a9d19d8a4ef77d6d491a86e92516
david <d8087869@gmail.com>
2026-07-12 12:21:43 +0300

    4-6

--- oop_basics_exe/basics_oop.py
+++ oop_basics_exe/basics_oop.py
@@ -29,3 +29,36 @@ menu3 = Menuitem('Cold Brew' , 5.0)
 menu1.describe()
 menu2.describe()
 menu3.describe()
+#4
+class Customers:
+    def __init__(self,name,balance):
+        self.name = name
+        self.balance = balance
+    def can_afford(self, price):
+        if self.balance >= price:
+            return True
+        else:
+            return False
+Customer2 = Customers('Bob',10.0)
+print(Customer2.can_afford(12))
+#5
+class Menuitem:
+    def __init__(self,name,price,in_stock):
+        self.name = name
+        self.price = price
+        self.in_stock = in_stock
+    def sell(self):
+            self.in_stock = False
+    def restock(self):
+        self.in_stock = True
+    def status(self):
+        if self.in_stock:
+            print(f'{self.name} is in stock')
+        else:
+            print(f'{self.name} is sold out')
+item = Menuitem("Muffin", 2.5, True)
+item.status()
+item.sell()
+item.status()
+item.restock()
+item.status()           
