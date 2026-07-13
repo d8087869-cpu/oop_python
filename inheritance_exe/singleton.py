898ebcaeb0c1b039606945bd648f99144fa5258a
david <d8087869@gmail.com>
2026-07-13 18:00:28 +0300

    1

---
+++ inheritance_exe/singleton.py
@@ -0,0 +1,22 @@
+#1 it ensuer that a class has exactly one instance . and that instance is shaerd 
+#2# only one because if will be few what will be the True one , and all are connected to one 
+#3  any part of the program can ask for the singleton and get back the same object
+#4 because its over rides the first calling and return existing info 
+#5 it could be change by other and evry thing depends on the singletone
+
+#1
+class Logger:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            print("Creating new Logger instance...")
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+logger1 = Logger()
+logger2 = Logger()
+
+print("Logger1:", id(logger1))
+print("Logger2:", id(logger2))
+print("Same object?", logger1 is logger2)
