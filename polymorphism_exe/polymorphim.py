29b5843d4e5ad551e1cbcf71f9619c1f896940d0
david <d8087869@gmail.com>
2026-07-14 12:19:42 +0300

    2-3

--- polymorphism_exe/polymorphim.py
+++ polymorphism_exe/polymorphim.py
@@ -1,3 +1,4 @@
+'''
 #1 
 class Device:
     def __init__(self,name):
@@ -14,3 +15,52 @@ tv = SmartTV('Samsung')
 tv.activate()
 speaker =SmartSpeaker('Echo')
 speaker.activate()
+#2
+class Device:
+    def __init__(self,name):
+        self.name= name
+    def deactivate(self):
+        print(f'Device {self.name} is now off.')
+class SmartLamp(Device):
+    def deactivate(self):
+        print(f'Lamp {self.name} is dimming and turning off.')
+class SmartAC(Device):
+    def deactivate(self):
+        print(f'AC {self.name} is cooling down and switching off.')
+lamp = SmartLamp('Bedroom Lamp')
+lamp.deactivate()
+ac = SmartAC('Living Room AC')
+ac.deactivate()        
+'''
+#3
+class Device:
+    def __init__(self,name,is_on):
+        self.name = name
+        self.is_on = is_on
+    def status(self):
+        if self.is_on:
+            print(f'{self.name}: on')
+        else:
+            print(f'{self.name}: off')
+class SamrtTV(Device):
+    def __init__(self, name, is_on,channel):
+        super().__init__(name, is_on)
+        self.channel = channel
+    def status(self):
+        if self.is_on:
+            print(f'{self.name} : on, watcing channel {self.channel}.')
+        else:
+            print(f'{self.name}: off')
+class SamrtSpeaker(Device):
+    def __init__(self, name, is_on,song):
+        super().__init__(name, is_on)
+        self.song = song
+    def status(self):
+        if self.is_on:
+            print(f'{self.name} : on, playing {self.song}.')
+        else:
+            print(f'{self.name}: off')
+tv = SamrtTV('LG',True,8)
+tv.status()
+speaker = SamrtSpeaker(f'Alexa',True,"Bohemian Rhapsody")
+speaker.status()
